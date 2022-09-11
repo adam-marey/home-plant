38b97c90a77b1c0ce2f6079b1c01aeb343e185ef
Sulaiman Marey <adammarey51@gmail.com>
2022-09-11 10:12:37 -0700

    added the product and all products component with styling

--- src/data.js
+++ src/data.js
@@ -43,34 +43,34 @@ export const categories = [
 export const popularProducts = [
   {
     id: 1,
-    img: 'https://images.pexels.com/photos/1084188/pexels-photo-1084188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559834442430-DBRWR0KGWCXAJFOUOG2S/giftcard-template.jpg?format=750w',
   },
   {
     id: 2,
-    img: 'https://images.pexels.com/photos/1179079/pexels-photo-1179079.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559833646039-FTYEXWSDNPJG79BCWWQQ/Ecomm_18_Palm_004.jpg?format=750w',
   },
   {
     id: 3,
-    img: 'https://images.pexels.com/photos/1931143/pexels-photo-1931143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559833414098-VOZ0QVGIQDABJ1MZA0I7/Ecomm_13_MaidenHair_006.jpg?format=750w',
   },
   {
     id: 4,
-    img: 'https://images.pexels.com/photos/2248529/pexels-photo-2248529.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559327213969-3LILJM6OQFB49I6HBOT8/Ecomm_03_CalatheaPink_006.jpg?format=750w',
   },
   {
     id: 5,
-    img: 'https://images.pexels.com/photos/2249959/pexels-photo-2249959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559327121147-RG1ANONPMAAXJKDWRODD/Ecomm_04_Orchid_005.jpg?format=750w',
   },
   {
     id: 6,
-    img: 'https://images.pexels.com/photos/1856430/pexels-photo-1856430.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559326971697-R6LD4Q9Z5V1EU4496JRZ/Ecomm_17_Snake_001.jpg?format=750w',
   },
   {
     id: 7,
-    img: 'https://images.pexels.com/photos/1424672/pexels-photo-1424672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559326925194-SH58FE5Q44V6AO38THEH/Ecomm_05_Alocasia_008.jpg?format=750w',
   },
   {
     id: 8,
-    img: 'https://images.pexels.com/photos/1328764/pexels-photo-1328764.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559327121147-RG1ANONPMAAXJKDWRODD/Ecomm_04_Orchid_005.jpg?format=750w',
   },
 ];
